43d2fd2579c5e44d9c32d204f81638f0e9aa4773
Galya <galina.bartkova.mir@gmail.com>
2020-10-07 16:16:29 +0300

    Timer

--- dist/js/main.js
+++ dist/js/main.js
@@ -90,4 +90,66 @@ window.addEventListener("scroll", showModalByScroll);
             });
         }
     });
+
+// Timer
+
+    const deadline = "2021-01-01";
+
+    function getTimeRemaining(endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        const days = Math.floor(t / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((t / (1000 * 60 * 60) % 24));
+        const minutes = Math.floor((t / 1000 / 60) % 60);
+        const seconds = Math.floor((t / 1000) % 60);
+
+        return{
+            "total" : t,
+            "days" : days,
+            "hours" : hours,
+            "minutes" : minutes,
+            "seconds" : seconds
+        };
+    }
+
+    function getZero(num){
+        if(num >= 0 && num < 10){
+            return `0${num}`;
+        }else{
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector);
+        const days = timer.querySelector("#days");
+        const hours = timer.querySelector("#hours");
+        const minutes = timer.querySelector("#minutes");
+        const seconds = timer.querySelector("#seconds");
+        timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+        
+        function updateClock(){
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+            // days.innerHTML = t.days;
+            // hours.innerHTML = t.hours;
+            // minutes.innerHTML = t.minutes;
+            // seconds.innerHTML = t.seconds;
+
+            if(t.total <= 0){
+                clearInterval(timeInterval);
+                days.innerHTML = "0";
+                hours.innerHTML = "0";
+                minutes.innerHTML = "0";
+                seconds.innerHTML = "0";
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 });
